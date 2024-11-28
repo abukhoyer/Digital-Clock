9fa1a191800ee4f8d63a45643af7961019ff1ced
Md. Abu khoyer <78016280+abukhoyer@users.noreply.github.com>
2024-11-28 16:09:08 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const time=document.getElementById('time');
+const timeFormat= document.getElementById('timeFormat');
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    setInterval(showTime, 1000);
+});
+
+const showTime = ()=>{
+    let date = new Date();
+    let hr=date.getHours();
+    let min=date.getMinutes();
+    let secs =date.getSeconds();
+    hr=hr<10 ? `0${hr}` : hr;
+    min=min<10 ?` 0${min}` : min;
+    secs=secs<10 ? `0${secs}` : secs;
+
+    time.innerHTML= `${ hr} : ${min} : ${secs}`;
+    timeFormat.innerHTML= hr>12 ? 'PM' : 'AM';
+    // console.log('hours' + hr + 'mins' + min + 'sec'+secs);
+    
+}
